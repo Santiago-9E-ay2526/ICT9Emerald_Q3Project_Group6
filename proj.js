87f005d2697e16442fbc12e1496e40263304df3d
vistro-9e-ay2526 <vistro.pi@obmontessori.edu.ph>
2026-03-12 15:51:14 +0800

    Update proj.js

--- proj.js
+++ proj.js
@@ -25,3 +25,33 @@ function buttonDiscount() {
             Discounted Fare (20% off) for ${pickup} to ${dropoff}: <strong>PHP ${discountedFare.toFixed(2)}</strong>
         </div>`;
 }
+
+// Password confirmation
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const password = document.getElementById("inputPassword");
+  const confirmPassword = document.getElementById("confirmPassword");
+
+  form.addEventListener("submit", function (event) {
+    if (password.value !== confirmPassword.value) {
+      event.preventDefault(); // stop form submission
+      alert("Passwords do not match!");
+    }
+  });
+
+// Image preview
+  const fileInput = document.getElementById("ChooseFile");
+  const profileImg = document.getElementById("ProfileImg");
+
+  fileInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        profileImg.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+});
+
